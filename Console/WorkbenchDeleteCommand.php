9e2e8d4d8db965d7090daa6868025b06d0b3f067
JackieDo <anhvudo@gmail.com>
2020-02-17 16:21:47 +0700

    Update WorkbenchDeleteCommand.php

--- Console/WorkbenchDeleteCommand.php
+++ Console/WorkbenchDeleteCommand.php
@@ -61,9 +61,8 @@ class WorkbenchDeleteCommand extends Command
             }
 
             Starter::removeDiscoveredPackage($this->package);
+            $this->info('>>> Workbench package "'.$this->package.'" has been deleted.');
         }
-
-        $this->info('>>> Workbench package "'.$this->package.'" has been deleted.');
     }
 
     /**
